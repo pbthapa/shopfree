5155855a5b06267f027dd725af859f2cb7d10069
pbthapa <ptpbthapa24@gmail.com>
2016-09-12 23:07:18 +0545

    @em is null

--- retail-ejb/src/main/java/com/retail/services/ejb/ProductServiceBean.java
+++ retail-ejb/src/main/java/com/retail/services/ejb/ProductServiceBean.java
@@ -12,14 +12,21 @@ import com.retail.services.remote.ProductService;
 @Stateless(name = "ProductService")
 public class ProductServiceBean implements ProductService {
 	
-//	@PersistenceContext(unitName = "retailPersistenceUnit")
-//	private static EntityManager em;
+	@PersistenceContext(unitName = "retailPersistenceUnit")
+	private static EntityManager em;
 
 	public String doProcess(String value) {
 
-//		Query query = em.createNativeQuery("select * from customer");
-//		List result = query.getResultList();
-//		System.out.println(result.get(0));
+		System.out.println("Called upto here--1--------------------->");
+		if(em == null) {
+			System.out.println("What the hell!!!, entity manager is null you noob");
+		}
+		Query query = em.createNativeQuery("select * from testtable");
+		System.out.println("Called upto here---2-------------------->");
+		List result = query.getResultList();
+		System.out.println("Called upto here-----3------------------>");
+		System.out.println(result.get(0));
+		System.out.println("Called upto here--------4--------------->");
 		return "Heello " + value + "!!!!!!. Result from DB : ";
 
 	}
